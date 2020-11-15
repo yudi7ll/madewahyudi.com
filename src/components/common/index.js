3af1d2fd0e4090a9c74e5bca8459be45a949b13e
Yudi <madewahyudi22@gmail.com>
2020-11-15 16:48:10 +0800

    export container

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,8 +1,10 @@
+import Container from './Container';
 import Header from './Header';
 import Layout from './Layout';
 import Seo from './Seo';
 
 export {
+  Container,
   Header,
   Layout,
   Seo,
