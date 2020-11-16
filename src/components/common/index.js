2fcb871fd4cca18c08f5e30a2b5bb483bebaa7cd
Yudi <madewahyudi22@gmail.com>
2020-11-16 17:42:33 +0800

    remove imported icons from components/common

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,13 +1,11 @@
 import Container from './Container';
 import Header from './Header';
-import * as Icons from './Icons';
 import Layout from './Layout';
 import Seo from './Seo';
 
 export {
   Container,
   Header,
-  Icons,
   Layout,
   Seo,
 }
