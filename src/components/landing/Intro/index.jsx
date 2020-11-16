509c19cbb8c200c02944b5b0fd31aaac8ff10132
Yudi <madewahyudi22@gmail.com>
2020-11-16 17:39:15 +0800

    update icons struct

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -9,9 +9,12 @@ import {
 } from './styles';
 
 // icons 
-import { Icons } from 'components/common';
+import { 
+  GithubIcon,
+  InstagramIcon,
+} from 'components/icons';
 
-function Sosmed({ children, to }) {
+function Sosmed({ Icon, to }) {
   return (
       <Link
         target="_blank"
@@ -21,7 +24,11 @@ function Sosmed({ children, to }) {
           marginRight: '.8em'
         }}
       >
-        { children }
+        <Icon
+          fill="#fff"
+          height="35px"
+          width="35px"
+        />
       </Link>
   );
 }
@@ -31,20 +38,14 @@ function Intro() {
     <Wrapper>
       <Heading1>Hello!, I'm Yudi</Heading1>
       <Bio>I’m a Web Developer from Bali Indonesia. Here I’ll make a website for you whether if it’s a Company Profile, Personal Website, Blog, etc.</Bio>
-      <Sosmed to="https://github.com/yudi7ll">
-        <Icons.GithubIcon
-          fill="#fff"
-          height="35px"
-          width="35px"
-        />
-      </Sosmed>
-      <Sosmed to="https://instagram.com/yudi_ll">
-        <Icons.InstagramIcon
-          fill="#fff"
-          height="35px"
-          width="35px"
-        />
-      </Sosmed>
+      <Sosmed
+        to="https://github.com/yudi7ll" 
+        Icon={GithubIcon}
+      />
+      <Sosmed
+        to="https://instagram.com/yudi_ll"
+        Icon={InstagramIcon}
+      />
     </Wrapper>
   );
 }
