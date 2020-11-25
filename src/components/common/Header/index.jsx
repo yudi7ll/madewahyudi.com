5bddb658aaa2f1a9a7f94af358c168942f42a130
Yudi <madewahyudi22@gmail.com>
2020-11-25 14:21:43 +0800

    update header

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -1,10 +1,10 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useRef, useEffect } from "react"
 import toSlugCase from "to-slug-case"
 import toTitleCase from "to-title-case"
 
-import { Brand, HeaderWrapper, Menu, Wrapper } from "./styles"
+import "./styles.scss";
 
 function MenuLink({ to }) {
   return (
@@ -25,17 +25,49 @@ function MenuLink({ to }) {
 }
 
 function Header() {
+  const nav = useRef(null);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        nav.current.classList.add('navbar-light', 'bg-light');
+        nav.current.classList.remove('mt-5', 'navbar-dark')
+      } else {
+        nav.current.classList.remove('bg-light', 'navbar-light');
+        nav.current.classList.add('mt-5', 'navbar-dark');
+      }
+    }
+  }, []);
+
   return (
-    <Wrapper>
-      <HeaderWrapper>
-        <MenuLink to="home" />
-        <MenuLink to="about" />
-        <MenuLink to="resume" />
-        <MenuLink to="portfolio" />
-        <MenuLink to="contact" />
-      </HeaderWrapper>
-    </Wrapper>
-  )
+    <nav
+      class="navbar navbar-expand-md navbar-dark fixed-top mt-5"
+      ref={nav}
+    >
+      <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#header" aria-controls="header" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="header">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item mx-3">
+            <a class="nav-link font-weight-bold" href="#">HOME</a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link font-weight-bold" href="#">ABOUT</a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link font-weight-bold" href="#">RESUME</a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link font-weight-bold" href="#">PORTFOLIO</a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link font-weight-bold" href="#">CONTACT</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Header
