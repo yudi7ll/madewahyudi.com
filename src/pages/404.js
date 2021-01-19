98d4e8d0f9f8c3e07f2badd4861b085c05c20c40
Yudi <madewahyudi22@gmail.com>
2021-01-19 19:19:58 +0800

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,10 @@ import { Layout, Seo } from "components/common"
 
 export default () => (
   <Layout>
-    <Seo title="404: Not Found" />
-    <h1>Not Found</h1>
-    <p>You just hit the route that doesn't exist... :(</p>
+    <Seo title="404 Not Found" />
+    <div className="p-5">
+      <h1><span className="color--red">404</span>: NOT FOUND</h1>
+      <p>You just hit the route that doesn't exist... :(</p>
+    </div>
   </Layout>
-)
+);
