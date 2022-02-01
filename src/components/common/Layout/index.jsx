60594e0d81b47fd4a62ce415e6112eb1a0fa5e54
Yudi <madewahyudi22@gmail.com>
2022-02-01 23:42:09 +0800

    add google analytics

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -9,7 +9,20 @@ const Layout = ({ children }) => (
       href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Anton&display=swap"
       rel="stylesheet"
     />
-    <main>{children}</main>
+    {/* eslint-disable-next-line react/no-danger */ }
+    <div dangerouslySetInnerHTML={{
+      __html: `
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-LNZQGS8EKR"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LNZQGS8EKR');
+          </script>
+        `,
+    }}
+    />
+    <main>{ children }</main>
   </>
 );
 
