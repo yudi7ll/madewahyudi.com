d50bf2a13763c3dfcd6ba9521a3b318070d8b655
Yudi <madewahyudi22@gmail.com>
2022-05-28 10:16:32 +0800

    Revert "add some particle (not tested)"
    This reverts commit 7838f93df7f627a496f4711f44701e4abc551149.

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -10,7 +10,7 @@ const Layout = ({ children }) => (
       rel="stylesheet"
     />
     {/* eslint-disable-next-line react/no-danger */ }
-    <div dangerouslySetInnerHTML={ {
+    <div dangerouslySetInnerHTML={{
       __html: `
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-LNZQGS8EKR"></script>
           <script>
@@ -20,20 +20,9 @@ const Layout = ({ children }) => (
             gtag('config', 'G-LNZQGS8EKR');
           </script>
         `,
-    } }
+    }}
     />
-    <main>
-      <>
-        { children }
-        {
-          Array.from(Array(10)).map(() => {
-            <div className="circle-container">
-              <div className="circle" />
-            </div>
-          })
-        }
-      </>
-    </main>
+    <main>{ children }</main>
   </>
 );
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,17 +12,14 @@ import {
 import 'components/styles/globals.scss';
 
 const IndexPage = () => (
-  <>
-    <canvas id="ParticleCanvas" styles="position: absolute; top: 0; bottom: 0; left: 0; right: 0;" />
-    <Layout>
-      <Seo title="Yudi" />
-      <Intro />
-      <Portfolio />
-      <MyTools />
-      <Contact />
-      <Footer />
-    </Layout>
-  </>
+  <Layout>
+    <Seo title="Yudi" />
+    <Intro />
+    <Portfolio />
+    <MyTools />
+    <Contact />
+    <Footer />
+  </Layout>
 );
 
 export default IndexPage;
