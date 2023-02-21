ecbbf27b900568c5634f310cbbec4d90a4d8446a
Yudi <madewahyudi22@gmail.com>
2023-02-21 16:26:44 +0800

    refactor portfolio component

--- src/components/landing/Portfolio/index.tsx
+++ src/components/landing/Portfolio/index.tsx
@@ -1,6 +1,5 @@
-import { PageProps } from 'gatsby'
 import { IGatsbyImageData } from 'gatsby-plugin-image'
-import React from 'react'
+import React, { HTMLAttributes } from 'react'
 import OpenSource from './OpenSource'
 import Proprietary from './Proprietary'
 import './styles.scss'
@@ -29,33 +28,37 @@ export interface IAllProprietaryJsonGraphql {
   }
 }
 
-const Portfolio: React.FC<PageProps> = (props) => {
-  console.log(props)
-  return (
-    <div className="mt-36" id="portfolio">
-      <div className="container">
-        <h2 className="text-header text-center uppercase">
-          <span>check out my </span>
-          <span className="text-y-accent">works</span>
-        </h2>
-        <div className="flex flex-1 flex-wrap justify-center gap-4 mt-4">
-          <OpenSource />
-        </div>
-      </div>
+const Grid = ({ children }: HTMLAttributes<HTMLDivElement>) => (
+  <div className="flex flex-1 flex-wrap justify-center gap-4 mt-4">
+    {children}
+  </div>
+)
 
-      <div className="container portfolio">
-        <h2 className="text-header text-center uppercase">
-          <span className="text-y-accent">proprietary </span>
-          project
-        </h2>
-        <small className="flex justify-center" style={{ opacity: 0.5 }}>
-          (( Under construction ))
-        </small>
-        <div className="flex flex-1 flex-wrap justify-center gap-4 mt-4">
-          <Proprietary />
-        </div>
-      </div>
+const Portfolio = () => (
+  <div className="mt-36" id="portfolio">
+    <div className="container">
+      <h2 className="text-header text-center uppercase">
+        <span>check out my </span>
+        <span className="text-y-accent">works</span>
+      </h2>
+      <Grid>
+        <OpenSource />
+      </Grid>
     </div>
-  )
-}
+
+    <div className="container mt-24">
+      <h2 className="text-header text-center uppercase">
+        <span className="text-y-accent">proprietary </span>
+        project
+      </h2>
+      <small className="flex justify-center" style={{ opacity: 0.5 }}>
+        (( Under construction ))
+      </small>
+      <Grid>
+        <Proprietary />
+      </Grid>
+    </div>
+  </div>
+)
+
 export default Portfolio
