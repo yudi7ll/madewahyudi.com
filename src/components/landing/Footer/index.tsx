12c151e89ee7ede4fb9267d0cac4231aa7c66808
Yudi <madewahyudi22@gmail.com>
2023-02-21 17:46:57 +0800

    update the footer component

--- src/components/landing/Footer/index.tsx
+++ src/components/landing/Footer/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
-import './styles.scss'
 import { CircleIcon } from '../../icons'
 
 const Footer = () => (
-  <footer className="footer">
+  <footer className="mt-40">
     <div className="container">
       <div className="text-center text-white py-4">
-        <small className="text__xs">
+        <small className="text-xs flex items-center justify-center space-x-1">
           <span>&copy; Copyright </span>
           <span>{new Date().getFullYear()}</span>
           <a href="https://github.com/yudi7ll" target="_blank" rel="noreferrer">
-            {' '}
             Yudi
           </a>
           <CircleIcon className="mx-1" height="10px" width="10px" fill="#fff" />
-          Under development
+          <span>Under development</span>
         </small>
       </div>
     </div>
